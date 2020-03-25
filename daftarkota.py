66796df47dc1b44bd074816c56729bdae21a826d
Dohan Pranata W <dohanpw@gmail.com>
2020-03-25 15:11:14 +0700

    sdas

--- daftarkota.py
+++ daftarkota.py
@@ -3,3 +3,14 @@
 def daftarkota(name):
     listkota = []
     count = 0
+    for i in name:
+        listkota.append('-')
+        listkota.append(i)
+        count+=1
+
+        if count%4 == 0:
+            listkota.append('\n')
+
+    listkota = ' '.join(listkota)
+    print(listkota)
+
